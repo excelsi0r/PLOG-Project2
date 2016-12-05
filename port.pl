90287cd64564ede94d102b09603b7dd6f711f73c
excelsi0r <nuno_mgl@hotmail.com>
2016-12-05 21:23:01 +0000

    added cycle

--- port.pl
+++ port.pl
@@ -1,45 +1,83 @@
 :- include('config.pl').
 
+:- use_module(library(lists)).
+
 :- 	dynamic
 	port/1.
 	
 port(_).
 
-port_init:- 	
+port_init:- 
+
 		write('Loading Port Configurations...'), nl, nl,
 		
+		%retrieving config and display
 		port_lines(Lines),
 		port_columns(Columnns),
-		container_types(Types),
-		
+		container_types(Types),	
 		display_config(Lines, Columnns, Types),
 		
-		create_port(Lines, Columnns),
+		%creating port and display
+		create_port(Lines, Columnns),	
+		display_port_by_length,
 		
-		port(L),
-		write(L).
+		%start cycle to create boats with containers
+		start_cycle(Types).
 		
 
+%=========================================================================
 
+start_cycle(N):-	
+
+		get_str,
+		write('Next'),
+		start_cycle(N).
+
+get_str:-	
+			repeat,
+				get_char(C),
+				C = '\n'.
+	
+	
+
+%=========================================================================
 create_port(Lines, Columnns):-	
 
 		create_port_line(Lines, Columnns, L),
-		assert(port(L)).
+		asserta(port(L)).
 		
 
 create_port_line(0,_,L):- 	L = [].
 create_port_line(Lines,Columnns,L):- 
 		
-		lentgh(L1, Columnns),
+		length(L1, Columnns),
+		maplist(=([]), L1),
 		N is Lines - 1,
 		create_port_line(N, Columnns, L2),
-		append(L1, L2, L).
+		append([L1], L2, L).
+		
+display_port_by_length:-	
 
+		port(L),
+		display_matrix_by_length(L).
 
-			
-			
+display_matrix_by_length([]):-	write('---------------------'), nl,nl.		
+display_matrix_by_length([Line | Rest]):-	
+
+		write('---------------------'), nl,
+		display_line_by_length(Line), nl,
+		display_matrix_by_length(Rest).
+		
+display_line_by_length([]):- write('|').
+display_line_by_length([Elem | Rest]):-	
+		
+		length(Elem, N),
+		write('| '), write(N), write(' '),
+		display_line_by_length(Rest).
+
 		
 		
+%=========================================================================
 display_config(Lines, Columnns, Types):-
 		
 		write('Port Lines: '), write(Lines), nl,
